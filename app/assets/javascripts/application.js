27b2dc7988786ecf49b5879eabd9ec273df1ba04
Alex Beznos <beznosa@yahoo.com>
2014-06-30 17:06:13 +0300

    Add create record template, route and controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,26 +5,39 @@
 //= require_self
 
 var app = Ember.Application.create({
-	LOG_TRANSITIONS: true
+  LOG_TRANSITIONS: true
 });
 
 
 
 // Controller
 app.TaskListsController = Ember.ArrayController.extend({
-	siteName: "TODO:"
+  siteName: "TODO:"
 });
 
 app.TasksController = Ember.ArrayController.extend({
-	sortProperties: ['description']
+  sortProperties: ['description']
+});
+
+app.TaskListsNewController = Ember.Controller.extend({
+  actions: {
+    createRecord: function() {
+      var controller = this;
+      this.get('model').save().then(function() {
+        controller.transitionToRoute('task_lists')
+      });
+    }
+  }
 });
 
 
 // Router
 app.Router.map(function(){
-	this.resource('task_lists', { path: '/'}),
-	this.resource('task_list', { path: 'task_list/:id'}),
-	this.resource('task', { path: 'tasks/:task_id'})
+  this.resource('task_lists', { path: '/'}, function() {
+    this.route('new')
+  }),
+  this.resource('task_list', { path: 'task_list/:id'}),
+  this.resource('task', { path: 'tasks/:task_id'})
 });
 
 
@@ -32,23 +45,22 @@ app.Router.map(function(){
 
 // Routes
 
-app.TaskListsRoute = Ember.Route.extend({
-	model: function() {
-		return this.store.findAll('task_list');
-	}
+app.TaskListsNewRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('task_list')
+  }
 });
-
-app.TaskListRoute = Ember.Route.extend({
-	model: function(params) {
-		return this.store.find('task_list', params.id)
-	}
+app.TaskListsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.findAll('task_list');
+  }
 });
 
 
 app.TasksRoute = Ember.Route.extend({
-    afterModel: function() {
-        this.set('task_list', this.modelFor('task_list'));
-    }
+  afterModel: function() {
+    this.set('task_list', this.modelFor('task_list'));
+  }
 });
 
 
@@ -57,20 +69,20 @@ app.TasksRoute = Ember.Route.extend({
 // Model
 
 app.TaskList = DS.Model.extend({
-	name: DS.attr('string'),
-	task_ids: DS.hasMany('task', {async: true})
+  name: DS.attr('string'),
+  task_ids: DS.hasMany('task', {async: true})
 });
 
 app.Task = DS.Model.extend({
-	description: DS.attr('string'),
-	status: DS.attr('boolean'),
-	task_list_id: DS.belongsTo('taskList')
+  description: DS.attr('string'),
+  status: DS.attr('boolean'),
+  task_list_id: DS.belongsTo('taskList')
 });
 
 
 
 // Adapter
 DS.RESTAdapter.reopen({
-	namespace: 'api'
+  namespace: 'api'
 });
 
